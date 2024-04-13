187d979db955e2b5be4df9209bc639fde85bf177
Remnetorp <m.remnetorp@gmail.com>
2024-04-13 23:59:53 +0200

    Fixed the algorithm, program works now. Added more input files, also provided some txt files for expected answers.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Algorithm to solve finding the path between two places. Every place or node is a five-letter word. There exist a path from one node to another node if the last four letter in the first node exist in the second node. The same rules applies but in the other direction if there exists a path back from the second node to the first node. The program solving the algorithm should take a query containing a starting word and a ending word, and the program will return the length of the shortest path. 
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+The syntax of the input file should follow the following conditions: The first row of the input consists two integers N,Q, the number of words we consider and the number of queries. Then follows N lines containing one five-letter word each. After this Q lines follow containing two space-separated five-letter words each, which are the queries. You can also find answers to the input files in same folder as example data.
+
+To run the algorithm, use following lines:
+(1) $ javac WordLadders.java
+(2) $ java WordLadders < data-input/data1.in
+(3) $ java WordLadders < data-input/data5.in output
+
--- WordLadders.java
+++ WordLadders.java
@@ -1,101 +1,189 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
 
-public class WordLadders{
+public class WordLadders {
     private int nQueries, nWords;
-    private String word,start,end;
-    private ArrayList<String> words;
-    private Map<String,ArrayList<String>> queries;
-    private Set<String> keySet;
+    private Set<String> words;
+    private Map<String, ArrayList<String>> possiblePaths;
+    private Map<Integer, ArrayList<String>> queries;
 
-    private void readFile(boolean output){
+    /**
+     * Method readInput reads an .in file and put the data into multiple collections
+     * later used in algorithm.
+     * 
+     * The first row of the input consists two integers N,Q, the number of words we
+     * consider and the number of queries.
+     * Then follows N lines containing one five-letter word each. After this Q lines
+     * follow containing two space-separated five-letter words each,
+     * which are the queries.
+     * 
+     * @param output Decideds if extra information should be printed out or not in
+     *               the terminal.
+     */
+    private void readInput(boolean output) {
         Scanner scanner = new Scanner(System.in);
         nWords = scanner.nextInt();
         nQueries = scanner.nextInt();
         scanner.nextLine();
-        words = new ArrayList<>();
+        words = new HashSet<>();
+        possiblePaths = new HashMap<>();
         queries = new HashMap<>();
 
-        for(int i=0; i < nWords; i++){
+        if (output) {
+            System.out.println("Number of words: " + nWords);
+            System.out.println("Number of queries: " + nQueries + "\n");
+            System.out.println("Words and queries on following lines:");
+        }
+
+        for (int i = 0; i < nWords; i++) {
             String word = scanner.next();
             words.add(word);
+        }
+
+        for (String word : words) {
+            ArrayList<String> possible = possiblePaths(word);
+            possiblePaths.put(word, possiblePaths(word));
+            if (output) {
+                System.out.println(word);
             }
+        }
+
         scanner.nextLine();
-        for(int j=0; j < nQueries; j++){
+        for (int j = 1; j < nQueries + 1; j++) {
             String in = scanner.nextLine();
             String[] input = in.split(" ");
-            start = input[0];
-            end = input[1];
-            if(queries.containsKey(start)){
-                queries.get(start).add(end);
-            }else{
-                ArrayList<String> ends = new ArrayList<>();
-                ends.add(end);
-                queries.put(start, ends);
+            ArrayList<String> query = new ArrayList<>();
+            query.add(input[0]);
+            query.add(input[1]);
+            queries.put(j, query);
+            if (output) {
+                System.out.println(queries.get(j).get(0) + " to " + queries.get(j).get(1));
             }
-            
         }
-        keySet = queries.keySet();
+        scanner.close();
+    }
 
-        if(output){
-            System.out.println("Number of words: " + nWords);
-            System.out.println("Number of queries: " + nQueries +"\n");
-            System.out.println("Words:");
-            for (int w = 0; w < words.size(); w++){
-                System.out.println(words.get(w) + " ");
+    /**
+     * This is a simple method calling the actual BTS algorithm method for every
+     * query received from the
+     * read method. After getting the results the method decides what to print.
+     * 
+     * @param output boolean to decide whether to print extra output information or
+     *               not.
+     */
+    private void queries(boolean output) {
+        for (int i = 1; i < queries.size() + 1; i++) {
+            ArrayList<String> query = queries.get(i);
+            int pathlength = shortestPath(query.get(0), query.get(1));
+            String s = query.get(0) + " --> " + query.get(1) + ": ";
+            if (output) {
+                if (pathlength == -1) {
+                    s += "Impossible";
+                } else {
+                    s += pathlength;
+                }
+            } else {
+                if (pathlength == -1) {
+                    s = "Impossible";
+                } else {
+                    s = String.valueOf(pathlength);
+                }
             }
-            System.out.println("\nQueries:");
-            for(String w2 : keySet){
-                System.out.println(w2 + " to " + queries.get(w2));
-            }
+            System.out.println(s);
         }
     }
 
+    /**
+     * shortestPath method is the method describing Breadth-first search algorithm,
+     * going through
+     * each word connected to current word and then increasing the path length until
+     * it finds the endword
+     * or not.
+     * 
+     * @param startWord a String representing the start of the query.
+     * @param endWord   a String representing the end of the query.
+     * @return return a Integer which is the shortest path from start to end.
+     */
+    private int shortestPath(String startWord, String endWord) {
+        LinkedList<String> queue = new LinkedList<>();
+        Set<String> visitedWords = new HashSet<String>();
+        queue.add(startWord);
+        visitedWords.add(startWord);
+        int pathLength = 0;
 
-    public int wordAlgorithm(){
-        int pathLength;
-        //for(String start : keySet){
-            pathLength = shortestPath("hello", "putin", words, 0);
-            System.out.println("Shortest path: " + pathLength);
-        //}
-
-        return 0;
-    }
-
-    private int shortestPath(String currentWord, String finalWord, ArrayList<String> paths, int pathLength){
-        paths.remove(currentWord);
-        if(currentWord.equals(finalWord)){
-            return pathLength;
-        }else{
-            String[] letters = currentWord.substring(currentWord.length()-4, currentWord.length()).split("");
-            for(String path : paths){
-                System.out.print(path);
-                Boolean containsLetters = true;
-                for(String letter : letters){
-                    if(!path.contains(letter)){
-                        containsLetters = false;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                String currentWord = queue.remove();
+                if (currentWord.equals(endWord)) {
+                    return pathLength;
+                } else {
+                    for (String path : possiblePaths.get(currentWord)) {
+                        if (visitedWords.add(path)) {
+                            queue.addLast(path);
+                        }
                     }
                 }
-                if(containsLetters){
-                    return shortestPath(path, finalWord, paths ,pathLength+1);
-                }
             }
-            return 10;
+            pathLength += 1;
         }
+        return -1;
     }
 
-    public static void main(String[] args){
+    /**
+     * The possiblePaths method is a method to fill a data collection, more
+     * specifically a map, with all the possible paths
+     * you can go from a certain word.
+     * 
+     * @param currentWord a String representing the word to consider when finding
+     *                    paths.
+     * @return a List containing all the Strings meeting the condition.
+     */
+    private ArrayList<String> possiblePaths(String currentWord) {
+        ArrayList<String> paths = new ArrayList<>();
+        String[] letters = currentWord.substring(currentWord.length() - 4, currentWord.length()).split("");
+        Map<String, Integer> letterCount = new HashMap<>();
+
+        for (String letter : letters) {
+            letterCount.put(letter, letterCount.getOrDefault(letter, 0) + 1);
+        }
+
+        for (String path : words) {
+            Map<String, Integer> pathLetters = new HashMap<>();
+            for (String letter : path.split("")) {
+                pathLetters.put(letter, pathLetters.getOrDefault(letter, 0) + 1);
+            }
+
+            boolean containsLetters = true;
+            for (String letter : letters) {
+                if (pathLetters.getOrDefault(letter, 0) < letterCount.get(letter)) {
+                    containsLetters = false;
+                    break;
+                }
+            }
+            if (containsLetters) {
+                paths.add(path);
+            }
+        }
+        paths.remove(currentWord);
+        return paths;
+    }
+
+    public static void main(String[] args) {
         WordLadders program = new WordLadders();
         boolean output = false;
-        for(String arg : args){
-            if (arg.equals("output")){
+        for (String arg : args) {
+            if (arg.equals("output")) {
                 output = true;
             }
         }
-        program.readFile(output);
-        int hej = program.wordAlgorithm();
+        program.readInput(output);
+        program.queries(output);
     }
 }
